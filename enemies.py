b58753a1f7d974b8b0f4811f74af46bee68419f5
dashcaj <dustinc7@nycstudents.net>
2024-04-18 14:00:33 -0400

    start

---
+++ enemies.py
@@ -0,0 +1,9 @@
+#dustin
+#all enemies:goomba, kevin, whalen, henriques, dean, 
+#caseoh, spongebob, The Jrim Reeper, Ted Bundy
+#hurricane sandy, lebron james
+#health, coins, exp,type, name, what level (training district)
+
+class enemies:
+    def __init__():
+        
