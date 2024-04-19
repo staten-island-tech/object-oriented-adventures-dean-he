c34bee7ae3847610328567e2b2bf3b2e8cee3942
dashcaj <dustinc7@nycstudents.net>
2024-04-19 14:00:52 -0400

    gg

--- enemies.py
+++ enemies.py
@@ -3,7 +3,64 @@
 #caseoh, spongebob, The Jrim Reeper, Ted Bundy
 #hurricane sandy, lebron james
 #health, coins, exp,type, name, what level (training district)
+import random
+
+""" def __init__(self, goomba, kevin, whalen, henriques, dean, caseoh, spongebob, The_Jrim_Reeper, Ted_Bundy, hurricane_sandy, Lebron_james):
+        self.goomba = goomba
+        self.kevin = kevin
+        self.whalen = whalen
+        self.henriques = henriques
+        self.dean = dean
+        self.caseoh = caseoh
+        self.spongebob = spongebob
+        self.The_Jrim_Reeper = The_Jrim_Reeper
+        self.Ted_Bundy = Ted_Bundy
+        self.hurricane_sandy = hurricane_sandy
+        self.Lebron_james = Lebron_james
+
+    def __str__(self): """
 
 class enemies:
-    def __init__():
-        
+    def Goomba():
+        health = random.choice[40, 60]
+        damage = random.choice[5, 15]
+        coins = 100
+        exp = random.choice[5, 10]
+        type = "Grass"
+        level = 1
+        boss = False
+
+    def Kevin():
+        health = random.choice[65, 90]
+        damage = random.choice[10, 15]
+        coins = 150
+        exp = random.choice[11, 20]
+        type = "Grass"
+        level = 1
+        boss = False
+
+    def Spongebob():
+        health = random.choice[95, 115]
+        damage = random.choice[16, 25]
+        coins = 200
+        exp = random.choice[21, 35]
+        type = "Water"
+        level = 1
+        boss = False
+
+    def Dean():
+        health = 500
+        damage = random.choice[25, 40]
+        coins = 400
+        exp = random.choice[50, 100]
+        type = "Grass"
+        level = 1
+        boss = True
+
+    
+
+    
+
+    
+    
+
