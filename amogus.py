de55d9706c0a397c5ac221a0a795defe2e1a83b9
Azrafdabomb <azrafh@nycstudents.net>
2024-05-03 13:25:35 -0400

    Create amogus.py

---
+++ amogus.py
@@ -0,0 +1,7 @@
+class player(name, health, moves, attack, level, money):
+    self.name = name
+    self.health = health
+    self.moves = moves
+    self.attack = attack
+    self.level = level
+    self.money = money
