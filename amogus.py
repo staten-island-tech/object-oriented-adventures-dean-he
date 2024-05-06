74ee47b74adc128f6b21317dd0a6ce7e28b5c829
Azrafdabomb <azrafh@nycstudents.net>
2024-05-06 13:37:14 -0400

    Update amogus.py

--- amogus.py
+++ amogus.py
@@ -1,9 +1,14 @@
-class player(name, health, moves, attack, level, money):
+class player(self, name, health, moves, attack, level, money):
     def name():
         name = input("Hello human, what will you like to be reffered to as?: ")
     def health():
-        health
+        health = playerhealth 
+
     def moves():
-        moves
+        movelist
     def attack():
-        player health - enemy damage
+        playerhealth - damage
+    def level():
+        level
+    def money():
+        howmuchmoney
