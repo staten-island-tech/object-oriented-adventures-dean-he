113071765bc7eddec90bedc0d438be8c43ade665
dashcaj <dustinc7@nycstudents.net>
2024-05-09 14:01:10 -0400

    ohio

--- districts.py
+++ districts.py
@@ -1,1 +1,16 @@
-print("soos")
+import json, random, time, os
+
+# Open the JSON file of pokemon data
+test = open("enemies.json", "uft8")
+# create variable "data" that represents the enitre pokedex list
+enemies = json.load(test)
+
+def District1():
+    enemys = []
+    for enemy in enemies:
+        if enemies["level"] == 1:
+            enemys.append["name"]
+    print(enemys)
+
+District1()
+
