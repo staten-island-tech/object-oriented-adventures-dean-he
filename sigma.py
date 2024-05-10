31ebdc0f80d88373cdf5460a1304caa4ea1db917
Azrafdabomb <azrafh@nycstudents.net>
2024-05-10 13:58:16 -0400

    f,gjfh

---
+++ sigma.py
@@ -0,0 +1,14 @@
+class player(self, name, health, moves, attack, level, money):
+    def name():
+        name = input("Hello human, what will you like to be reffered to as?: ")
+    def health():
+        health = playerhealth 
+
+    def moves():
+        movelist = available moves
+    def attack():
+        'attack system '
+    def level():
+        level
+    def money():
+        balance
