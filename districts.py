892eeeb27e9851192022fa3ca2249e1e0e06e5df
dashcaj <dustinc7@nycstudents.net>
2024-05-13 14:00:44 -0400

    Update districts.py

--- districts.py
+++ districts.py
@@ -1,16 +1,36 @@
 import json, random, time, os
+import player from amogus
 
 # Open the JSON file of pokemon data
 test = open("enemies.json", "uft8")
 # create variable "data" that represents the enitre pokedex list
 enemies = json.load(test)
 
+character = playe
+
 def District1():
     enemys = []
     for enemy in enemies:
         if enemies["level"] == 1:
-            enemys.append["name"]
-    print(enemys)
+            enemys.append["name", "health", "damage", "coins", "exp", "type", "level", "boss"]
+    select = random.choice[enemys]
+    print(select)
 
 District1()
+
+    
+    
+    
+
+def District2():
+    enemys = []
+    for enemy in enemies:
+        if enemies["level"] == 2:
+            enemys.append["name"]
+            
+def District3():
+    enemys = []
+    for enemy in enemies:
+        if enemies["level"] == 3:
+            enemys.append["name"]
 
