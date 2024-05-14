8e7868e861d441596abb918e1570bfb40f5886d2
dashcaj <dustinc7@nycstudents.net>
2024-05-14 14:01:13 -0400

    q

--- districts.py
+++ districts.py
@@ -1,12 +1,10 @@
 import json, random, time, os
-import player from amogus
 
 # Open the JSON file of pokemon data
 test = open("enemies.json", "uft8")
 # create variable "data" that represents the enitre pokedex list
 enemies = json.load(test)
 
-character = playe
 
 def District1():
     enemys = []
---
+++ levelup.py
@@ -0,0 +1,73 @@
+
+import json, random, time, os
+
+# Open the JSON file of pokemon data
+test = open("enemies.json", "uft8")
+# create variable "data" that represents the enitre pokedex list
+enemies = json.load(test)
+
+player = 100
+player_xp = 0
+
+if enemy_defeat == True:
+    for enemy in enemies:
+        if enemies["coins"] == 100:
+            enemies["coins"] + player
+        if enemies["exp"] == 1:
+            enemies["exp"] + player_xp
+
+required_xp_1 = 40
+required_xp_2 = 100
+required_xp_3 = 150
+required_xp_4 = 300
+required_xp_5 = 500
+required_xp_6 = 800
+required_xp_7 = 1000
+required_xp_8 = 1300
+required_xp_9 = 1500
+required_xp_10 = 5000
+
+
+if player_xp > 40:
+    print("You have leveled up: Level 2")
+    #make it stop here (idk how to)
+    if player_xp > 100:
+        print("You have leveled up: Level 3")
+        #make it stop here (idk how to)
+        if player_xp > 150:
+            print("You have leveled up: Level 4")
+            #make it stop here (idk how to)
+            if player_xp > 300:
+                print("You have leveled up: Level 5")
+                #make it stop here (idk how to)
+                if player_xp > 500:
+                    print("You have leveled up: Level 6")
+                    #make it stop here (idk how to)
+                    if player_xp > 800:
+                        print("You have leveled up: Level 7")
+                        #make it stop here (idk how to)
+                        if player_xp > 1000:
+                            print("You have leveled up: Level 8")
+                            #make it stop here (idk how to)
+                            if player_xp > 1300:
+                                print("You have leveled up: Level 9")
+                                #make it stop here (idk how to)
+                                if player_xp > 1500:
+                                    print("You have leveled up: Level 10")
+                                    #make it stop here (idk how to)
+        
+        
+
+
+
+
+
+
+
+
+
+""" if enemy_defeat = true:
+    drop money for the ebneny and xp from the nemey 
+    add mpnery from enemy to player balkance add xp tp player xp bar level 
+    from omney in enemy 
+    from vlv 1 to 2  """
