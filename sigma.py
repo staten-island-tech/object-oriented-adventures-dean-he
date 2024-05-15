1ea050180e09d7ca1f39543051553cd6014720d6
Azrafdabomb <azrafh@nycstudents.net>
2024-05-15 13:59:25 -0400

    Update sigma.py

--- sigma.py
+++ sigma.py
@@ -1,14 +1,14 @@
+enemy_defeat = True
 class player(self, name, health, moves, attack, level, money):
-    def name():
-        name = input("Hello human, what will you like to be reffered to as?: ")
-    def health():
-        health = playerhealth 
+    def __init__(self, name, health, moves, attack, level,):
+        self.name = input("Hello human, what will you like to be reffered to as?: ")
+        self.health = playerhealth 
+        self.movelist = available_moves
+        self.playerhealth = playerhealth 
+        self.level = current_level
+        def __init__(money): 
+        self.money = 0
+        if enemy_defeat == True :
+            balance + coins in enemies
 
-    def moves():
-        movelist = available_moves
-    def attack():
-        attack_system
-    def level():
-        level
-    def money():
-        balance
+money()
