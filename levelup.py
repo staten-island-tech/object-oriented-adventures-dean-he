de4ad8d372d3c044e9c7be8c8bae67199fc92b2d
dashcaj <dustinc7@nycstudents.net>
2024-05-15 14:01:54 -0400

    Update levelup.py

--- levelup.py
+++ levelup.py
@@ -1,60 +1,62 @@
-
+""" 
 import json, random, time, os
 
 # Open the JSON file of pokemon data
 test = open("enemies.json", "uft8")
 # create variable "data" that represents the enitre pokedex list
-enemies = json.load(test)
+enemies = json.load(test) """
 
 player = 100
-player_xp = 0
+player_xp = int(input("enter xp: "))
+player_level = 1
 
-if enemy_defeat == True:
+""" if enemy_defeat == True:
     for enemy in enemies:
         if enemies["coins"] == 100:
             enemies["coins"] + player
         if enemies["exp"] == 1:
-            enemies["exp"] + player_xp
+            enemies["exp"] + player_xp """
 
-required_xp_1 = 40
-required_xp_2 = 100
-required_xp_3 = 150
-required_xp_4 = 300
-required_xp_5 = 500
-required_xp_6 = 800
-required_xp_7 = 1000
-required_xp_8 = 1300
-required_xp_9 = 1500
-required_xp_10 = 5000
+required_xp = 50
+
+ #debug
+def levelup():
+        if player_xp == required_xp:
+            print(f"You have leveled up: Level {player_level}")
+            50 + required_xp
+            player_level + 1
+        
+            if player_level == 3:
+                150 + required_xp
+            
+            if player_xp == required_xp:
+                print(f"You have leveled up: Level {player_level}")
+                150 + required_xp
+                player_level + 1
+        
+                if player_level == 6:
+                    300 + required_xp
+                
+                if player_xp == required_xp:
+                    print(f"You have leveled up: Level {player_level}")
+                    300 + required_xp
+                    player_level + 1
+        
+                    if player_level == 10:
+                        print("MAX LEVEL!")
+
+levelup()
+        
+        
 
 
-if player_xp > 40:
-    print("You have leveled up: Level 2")
-    #make it stop here (idk how to)
-    if player_xp > 100:
-        print("You have leveled up: Level 3")
-        #make it stop here (idk how to)
-        if player_xp > 150:
-            print("You have leveled up: Level 4")
-            #make it stop here (idk how to)
-            if player_xp > 300:
-                print("You have leveled up: Level 5")
-                #make it stop here (idk how to)
-                if player_xp > 500:
-                    print("You have leveled up: Level 6")
-                    #make it stop here (idk how to)
-                    if player_xp > 800:
-                        print("You have leveled up: Level 7")
-                        #make it stop here (idk how to)
-                        if player_xp > 1000:
-                            print("You have leveled up: Level 8")
-                            #make it stop here (idk how to)
-                            if player_xp > 1300:
-                                print("You have leveled up: Level 9")
-                                #make it stop here (idk how to)
-                                if player_xp > 1500:
-                                    print("You have leveled up: Level 10")
-                                    #make it stop here (idk how to)
+
+
+
+
+
+
+
         
         
 
