e76a72588f4b4bf90e963abe6702e8e755536803
Frnkc35 <frankiec35@nycstudents.net>
2024-05-16 13:55:55 -0400

    Update shop.py

--- shop.py
+++ shop.py
@@ -5,13 +5,40 @@ test = open("stuff.json", encoding="utf8")
 data = json.load(test)
 
 
+class shop:
 
-def shop1():
-    access = input('Would you like to use the shop?(Y/N): ')
-    if access == 'y' or 'Y':
-        x = '1'
-        for stuff in data:
-            if x == stuff["level"]:
-                print(stuff["level"])
+    def shop1():
+        access = input('Would you like to use the shop?(Y/N): ')
+        if access == 'y' or 'Y':
+            x = '1'
 
-shop1()
+            for stuff in data:
+                if x in stuff["level"]:
+                    print (stuff["name"])
+
+    def shop2():
+        access = input('Would you like to use the shop?(Y/N): ')
+        if access == 'y' or 'Y':
+            x = '2'
+
+            for stuff in data:
+                if x in stuff["level"]:
+                    print (stuff["name"])
+
+    def shop3():
+        access = input('Would you like to use the shop?(Y/N): ')
+        if access == 'y' or 'Y':
+            x = '3'
+
+            for stuff in data:
+                if x in stuff["level"]:
+                    print (stuff["name"])
+
+
+y = PlayerDistrict
+if PlayerDistrict == '1':
+    shop.shop1
+elif PlayerDistrict == '2':
+    shop.shop2
+elif PlayerDistrict == '3':
+    shop.shop3
