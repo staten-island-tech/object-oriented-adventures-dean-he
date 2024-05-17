a86faad7fcb038ae394364061c7c471f5217e39f
Azrafdabomb <azrafh@nycstudents.net>
2024-05-17 13:57:41 -0400

    Lock

--- amogus.py
+++ amogus.py
@@ -1,27 +1,57 @@
-<<<<<<< Updated upstream
-class player(self, name, health, moves, attack, level, money):
-    def name():
-        name = input("Hello human, what will you like to be reffered to as?: ")
-    def health():
-        health = playerhealth 
+class player:
+    def __init__(name, health, moves, attack, level, money, district, turn):
+        self.name = name
+        self.health = health
+        self.moves = moves
+        self.attack = attack
+        self.level = level
+        self.money = money
+        self.district = district
+        self.turn = turn
 
-    def moves():
-        movelist
-    def attack():
-        playerhealth - damage
-    def level():
-        level
-    def money():
-        balance = 0
-        if enemy_defeat == True :
-            
-=======
-class player(name, health, moves, attack, level, money):
-    self.name = name
-    self.health = health
-    self.moves = moves
-    self.attack = attack
-    self.level = level
-    self.money = money
+name = input(("Hello human, what would you like to be reffered to as?"))
+health = 100
+moves = Move_list
+attack = amount_of_damage
+level = 0
+money = 0
+district = 0 
+turn = 0 
+
+while battle_sequence == True:
+    while enemy_health > 0:
+        #Select random enemy from enemy list in current district
+        turn + 1 
+        while turn == odd:
+            print (#available moves)
+            player_attack = input("what move would you like to use?: ")
+            #if move is in list of moves:
+            #    preform the move on the enemy
+            #    turn + 1
+            #else:
+            #    print("Sorry that isn't a valid move... You can use these moves here: ")
+            #    print (#available moves)
+            #   player_attack = input("what move would you like to use?: ")
+        
+        while turn == even:
+            Allow the enemy to attack
+            turn + 
+      else:
+        print(enemy name' Has been defeeated! )   
     
->>>>>>> Stashed changes
+
+    
+
+
+class drink:
+    def __init__(self, name, calories, quantity, price, sugar):
+        self.name = name
+        self.calories = calories
+        self.quantity = quantity
+        self.price = price
+        self.sugar = sugar
+    def __str__(self):
+        return f"{self.name}{self.calories}{self.quantity}{self.price}{self.sugar}"
+
+
+
