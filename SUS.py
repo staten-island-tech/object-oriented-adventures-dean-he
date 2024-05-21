d785ec952cf990f86d239e2e404810065ff9745f
dashcaj <dustinc7@nycstudents.net>
2024-05-21 14:01:42 -0400

    Update SUS.py

--- SUS.py
+++ SUS.py
@@ -5,3 +5,32 @@ test = open("enemies.json", "uft8")
 # create variable "data" that represents the enitre pokedex list
 enemies = json.load(test)
 
+name = input(("Hello human, what would you like to be reffered to as?"))
+health = 100
+moves = Move_list
+attack = amount_of_damage
+level = 0
+money = 0
+district = 0 
+turn = 0 
+
+while battle_sequence == True:
+    while enemy_health > 0:
+        #Select random enemy from enemy list in current district
+        turn + 1 
+        while turn == odd:
+            print (#available moves)
+            player_attack = input("what move would you like to use?: ")
+            #if move is in list of moves:
+            #    preform the move on the enemy
+            #    turn + 1
+            #else:
+            #    print("Sorry that isn't a valid move... You can use these moves here: ")
+            #    print (#available moves)
+            #   player_attack = input("what move would you like to use?: ")
+        
+        while turn == even:
+            Allow the enemy to attack
+            turn + 
+      else:
+        print(enemy name' Has been defeeated! )  
