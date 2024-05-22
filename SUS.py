5e8a39a7d7291581cd50a8edd3fd6f7004bd152a
dashcaj <dustinc7@nycstudents.net>
2024-05-22 13:59:40 -0400

    Update SUS.py

--- SUS.py
+++ SUS.py
@@ -1,4 +1,6 @@
 import json, random, time, os
+from levelup import levelup
+from districts import districts
 
 # Open the JSON file of pokemon data
 test = open("enemies.json", "uft8")
@@ -6,15 +8,35 @@ test = open("enemies.json", "uft8")
 enemies = json.load(test)
 
 name = input(("Hello human, what would you like to be reffered to as?"))
+time.sleep(0.5)
+print("Welcome to THE ULTIMATE GAME. You will be playing a RPG Game. I will be gatekeeping the instruction from you.")
+time.sleep(0.5)
+print("JUST KIDDING! Muahahahahah.")
+print("You must fight against different enemies in different districts, Each time you level up, you get closer to unlocked the new district. You gain levels and bank into order to")
+print("get more attacks and stronger stats. YOu can choose between fighting, shopping, and healing.")
+time.sleep(0.5)
+print["You wake up, wooden sword in hand, destined to resist the NOT YET. "]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 health = 100
-moves = Move_list
-attack = amount_of_damage
 level = 0
 money = 0
-district = 0 
 turn = 0 
 
-while battle_sequence == True:
+""" while battle_sequence == True:
     while enemy_health > 0:
         #Select random enemy from enemy list in current district
         turn + 1 
@@ -33,4 +55,4 @@ while battle_sequence == True:
             Allow the enemy to attack
             turn + 
       else:
-        print(enemy name' Has been defeeated! )  
+        print(enemy name' Has been defeeated! )   """
