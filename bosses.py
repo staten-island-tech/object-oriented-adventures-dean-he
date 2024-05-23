0db19cb382316977952b99b6df579b5bddcca4b7
Azrafdabomb <azrafh@nycstudents.net>
2024-05-23 13:40:03 -0400

    swalalala

---
+++ bosses.py
@@ -0,0 +1,11 @@
+if key_1 == bought:
+    enemy = Henriques
+    battle_sequence = True
+
+if key_2 == bought:
+    enemy = dean
+    battle_sequence = True
+
+if key_3 == bought:
+    enemy = shadow
+    battle_sequence = True
