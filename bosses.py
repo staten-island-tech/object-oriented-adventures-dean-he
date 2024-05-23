66c98a6c916bfd9a3cf21269716e55f747fbb84e
Azrafdabomb <azrafh@nycstudents.net>
2024-05-23 13:57:28 -0400

    'fwouti

--- bosses.py
+++ bosses.py
@@ -9,3 +9,7 @@ if key_2 == bought:
 if key_3 == bought:
     enemy = shadow
     battle_sequence = True
+
+if shadow = "dead":
+    print("Someone's shadow: Ah, I see that you've become very stong on this journy of awesome legemdary mythical extravagant bodaicous epicness")
+
