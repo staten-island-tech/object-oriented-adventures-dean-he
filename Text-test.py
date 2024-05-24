48ecf3967b8261c6a4c3bcbbc3ebec6e3e2b2f5a
Azrafdabomb <azrafh@nycstudents.net>
2024-05-24 13:53:31 -0400

    sussus

---
+++ Text-test.py
@@ -0,0 +1,33 @@
+import time
+
+print("Someone's shadow: Ah, I see that you've become very stong on this journey of awesome legemdary mythical extravagant bodaicous epicness!")
+time.sleep(3.5)
+print("Too bad it has to end early!")
+time.sleep(2.5)
+time.sleep(1)
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(2)
+print("You've never been more deserving of a DEV at best anyways.") 
+time.sleep(2)
+print("I hate you, and you will amount to nothing in life, player_name.")
+time.sleep(5)
+print("You've fought my minions, and they have failed to deliver the NOT YET to your face.")
+time.sleep(4)
+print("Stupid minions weren't good for anything anyways!")
+time.sleep(2.5)
+print("Whalen's shadow: If they can't perform such a simple task...")
+time.sleep(3)
+print("Whalen: I'll just have to do it myself!")
+time.sleep(2)
+
+
+Enemy = Whalen
+battle_sequence = True
+
+
+
+
--- bosses.py
+++ bosses.py
@@ -11,5 +11,11 @@ if key_3 == bought:
     battle_sequence = True
 
 if shadow = "dead":
-    print("Someone's shadow: Ah, I see that you've become very stong on this journy of awesome legemdary mythical extravagant bodaicous epicness")
+    print("Someone's shadow: Ah, I see that you've become very stong on this journey of awesome legemdary mythical extravagant bodaicous epicness")
+    print("Someone's shadow: Well this is the end of the story")
+    print(".")
+    print(".")
+    print(".")
+    print("for you! I've always hated you player_name!") 
+    print("Whalen: ")
 
