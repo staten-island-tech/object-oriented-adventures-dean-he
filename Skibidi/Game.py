9d615b44a75ec17ae8f989f57f8c1fd808ac7069
Frnkc35 <frankiec35@nycstudents.net>
2024-06-04 10:44:07 -0400

    fasdsadsa

---
+++ Skibidi/Game.py
@@ -0,0 +1,15 @@
+import time
+
+class Gamne:
+    def StartMenu(self):
+        self.name = input(("Hello human, what would you like to be referred to as?: "))
+        time.sleep(0.5)
+        print(f"Welcome to THE ULTIMATE GAME, {self.name}. You will be playing a RPG Game. I will be gatekeeping the instruction from you.\n")
+        time.sleep(4)
+        print("JUST KIDDING! Muahahahahah.\n")
+        time.sleep(1)
+        print("You must fight against different enemies in different districts, Each time you level up, you get closer to unlocking the new district. You gain levels and bank into order to")
+        print("get more attacks and stronger stats. You can choose between fighting, shopping, and healing.\n")
+        time.sleep(10)
+        print("You wake up, GLOCK in hand, destined to resist the NOT YET. ")
+    
